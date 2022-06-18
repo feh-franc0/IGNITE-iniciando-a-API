19db7773c1339d3fc013aa5f9853e6cd64083a84
feh-franc0 <fernandofv1110@gmail.com>
2022-06-18 19:49:41 -0300

    Listando as categorias

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -8,9 +8,15 @@ const CategoriesRepository = new RCategoriesRepository();
 categoriesRoutes.post("/", (request, response) => {
     const { name, description } = request.body;
 
-    CategoriesRepository.create({ name, description })
+    CategoriesRepository.create({ name, description });
 
     return response.status(201).send();
-})
+});
+
+categoriesRoutes.get("/", (request, response) => {
+    const all = CategoriesRepository.list();
+
+    return response.json(all);
+});
 
 export { categoriesRoutes };
