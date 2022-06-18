4b171ac9e2de5f59bf9f45861fd7224e9d3bfa50
feh-franc0 <fernandofv1110@gmail.com>
2022-06-18 20:40:51 -0300

    Validando o cadastro de categoria

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -8,6 +8,12 @@ const CategoriesRepository = new RCategoriesRepository();
 categoriesRoutes.post("/", (request, response) => {
     const { name, description } = request.body;
 
+    const categoryAlreadyExists = CategoriesRepository.findByName(name);
+
+    if (categoryAlreadyExists) {
+        return response.status(400).json({ error: "Category Already Exists" })
+    }
+
     CategoriesRepository.create({ name, description });
 
     return response.status(201).send();
