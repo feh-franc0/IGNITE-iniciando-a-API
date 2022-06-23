586932df8face713a68e0902a3c98272a9a60a5c
feh-franc0 <fernandofv1110@gmail.com>
2022-06-23 02:34:49 -0300

    Criando repositório de especificação

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
 import express from "express";
+
+import { specificationsRoutes } from "./routes/specifications.routes";
 import { categoriesRoutes } from "./routes/categories.routes";
 
 const app = express();
@@ -6,5 +8,6 @@ const app = express();
 app.use(express.json())
 
 app.use("/categories", categoriesRoutes)
+app.use("/specifications", specificationsRoutes)
 
 app.listen(3333, () => console.log("Server is running!"));
